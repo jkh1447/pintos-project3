ccfe24dcdb7c9ac61dce0a0bf0be5ebda997dfb3
jkh1447 <jkh1447@gmail.com>
2025-06-09 23:06:40 +0900

    test

--- userprog/process.c
+++ userprog/process.c
@@ -1082,7 +1082,7 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
 		lsp->writable = writable;
 		aux = lsp;
 
-		// VM_FILE이 아닌가??
+		
 		if(writable){
 			if (!vm_alloc_page_with_initializer (VM_ANON, upage,
 					writable, lazy_load_segment, aux))
